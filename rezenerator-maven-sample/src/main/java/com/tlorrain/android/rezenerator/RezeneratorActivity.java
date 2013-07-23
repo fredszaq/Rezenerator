69224534dc6a4dfaeb3871f99a473788ec20b0eb
Thibaut Lorrain <fredszaq@gmail.com>
2013-07-24 00:41:22 +0200

    remove useless method in maven sample

--- rezenerator-maven-sample/src/main/java/com/tlorrain/android/rezenerator/RezeneratorActivity.java
+++ rezenerator-maven-sample/src/main/java/com/tlorrain/android/rezenerator/RezeneratorActivity.java
@@ -2,7 +2,6 @@ package com.tlorrain.android.rezenerator;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.widget.ImageView;
 
 public class RezeneratorActivity extends Activity {
@@ -13,12 +12,6 @@ public class RezeneratorActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		// you can reference icons as before
 		((ImageView) findViewById(R.id.im_prog)).setImageResource(R.drawable.ic_launcher);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater();
-		return true;
 	}
 
 }
