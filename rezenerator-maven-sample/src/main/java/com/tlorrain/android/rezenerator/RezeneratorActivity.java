efffcee407b2b231a193d1b204e84453d77dc770
Thibaut Lorrain <fredszaq@gmail.com>
2013-07-24 00:40:37 +0200

    added a few basic usages in maven sample project, close #15

--- rezenerator-maven-sample/src/main/java/com/tlorrain/android/rezenerator/RezeneratorActivity.java
+++ rezenerator-maven-sample/src/main/java/com/tlorrain/android/rezenerator/RezeneratorActivity.java
@@ -1,25 +1,24 @@
-
 package com.tlorrain.android.rezenerator;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ImageView;
 
-public class RezeneratorActivity
-    extends Activity
-{
+public class RezeneratorActivity extends Activity {
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// you can reference icons as before
+		((ImageView) findViewById(R.id.im_prog)).setImageResource(R.drawable.ic_launcher);
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater();
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater();
+		return true;
+	}
 
 }
