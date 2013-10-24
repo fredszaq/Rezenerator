37ec324b19b4727f4526f1ba91412753efc66453
Thibaut Lorrain <fredszaq@gmail.com>
2013-10-24 14:20:23 +0200

    add rezenerator.definition.dirs system property in standalone, close #23

--- rezenerator-standalone/src/main/java/com/tlorrain/android/rezenerator/standalone/Rezenerator.java
+++ rezenerator-standalone/src/main/java/com/tlorrain/android/rezenerator/standalone/Rezenerator.java
@@ -6,22 +6,29 @@ import com.tlorrain.android.rezenerator.core.Configuration;
 import com.tlorrain.android.rezenerator.core.RezeneratorRunner;
 
 public class Rezenerator {
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		if (args.length < 2) {
 			System.out.println("syntax <executable> inDir outDir");
 			return;
 		}
-		Configuration configuration = new Configuration();
+		final Configuration configuration = new Configuration();
 
 		configuration.setInDir(new File(args[0]))//
 				.setBaseOutDir(new File(args[1]))//
 				.addScannedPackage("com.tlorrain.android")//
 				.setLogger(new PrintStreamLogger(System.out, System.err));
 
-		String pakages = System.getProperties().getProperty("rezenerator.scanned.packages");
-		if (pakages != null) {
-			for (String pkg : pakages.split(",")) {
+		final String packages = System.getProperties().getProperty("rezenerator.scanned.packages");
+		if (packages != null) {
+			for (final String pkg : packages.split(",")) {
 				configuration.addScannedPackage(pkg);
+			}
+
+		}
+		final String definitionDirs = System.getProperties().getProperty("rezenerator.definition.dirs");
+		if (definitionDirs != null) {
+			for (final String def : definitionDirs.split(",")) {
+				configuration.addDefinitionDir(new File(def));
 			}
 
 		}
